e346a4d6df40563d373506188bb4b5dcff07897d
mariantaflou <150686219+mariantaflou@users.noreply.github.com>
2024-12-31 19:04:30 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:journito/screens/information_screen.dart';
+//import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'screens/welcome_screen.dart';
-import 'screens/login.dart'; // Import the login screen
-import 'screens/home_screen.dart';
 import 'screens/ai_chat.dart';
-import 'screens/tasks_screen.dart'; // Add this line
 import 'screens/calendar.dart';
+import 'screens/home_screen.dart';
 import 'screens/journaling.dart';
+import 'screens/login.dart';
+import 'screens/tasks_screen.dart';
+import 'screens/experiences_screen.dart';
 
-
-void main() {
-  runApp(const MyApp());
+void main() async {
+  //WidgetsFlutterBinding.ensureInitialized();
+  //await dotenv.load();
+  //final apiKey = dotenv.env['GEMINI_API_KEY'];
+  //print('Loaded API Key: $apiKey'); // Check the value of apiKey
+  //if (apiKey == null || apiKey.isEmpty) {
+  //  print('Error: GEMINI_API_KEY is not set in the .env file.');
+  //}
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,14 +31,18 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => const WelcomeScreen(),
-        '/login': (context) => const LoginScreen(),
-        '/home': (context) => const HomeScreen(), // Define the home screen route
-        '/ai_chat': (context) => const ChatWithAIScreen(),
-        '/tasks': (context) => const TasksScreen(),
-        '/calendar': (context) => const CalendarScreen(),
-        '/journaling': (context) => const JournalingScreen(),
+      '/': (context) => const WelcomeScreen(),
+      '/login': (context) => const LoginScreen(),
+      '/home': (context) => const HomeScreen(),
+      '/ai_chat': (context) => const ChatWithAIScreen(),
+     '/tasks': (context) => const TasksScreen(),
+     '/calendar': (context) => const CalendarScreen(),
+     '/journaling': (context) => const JournalingScreen(),
+      '/experiences': (context) => const ExperiencesScreen(),
+      '/information': (context) => const InformationScreen(),
+
       },
+
     );
   }
 }
