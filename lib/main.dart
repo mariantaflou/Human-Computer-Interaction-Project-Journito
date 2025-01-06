02a57154dd5ca7bd6bca6f3a9cd49ea2dc743158
Maria Ntaflou <150686219+mariantaflou@users.noreply.github.com>
2025-01-06 21:39:44 +0200

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,9 @@ import 'screens/journaling.dart';
 import 'screens/login.dart';
 import 'screens/tasks_screen.dart';
 import 'screens/experiences_screen.dart';
-import 'screens/preferences_screen.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
+import 'screens/preferences_screen.dart'; // Import the PreferencesScreen
 
 void main() async {
-  await dotenv.load();
   runApp(MyApp());
 }
 
